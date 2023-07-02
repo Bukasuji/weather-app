b18bf8985aa68257cf4ea50189bf9032018db494
Chukwuebuka <chukwuebukaosuji@gmail.com>
2023-07-02 18:39:12 +0100

    added spinner

---
+++ app/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Spinner = () => {
+    return(
+        <>
+           <img src="/Spinner.gif" alt="loading" className='w-[200px] m-auto block' />
+        </>
+    )
+}
+
+export default Spinner
